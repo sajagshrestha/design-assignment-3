0c70071b8dea80b1c01a923125de3ba6531b061e
sajagshrestha <sajagshrestha0852@gmail.com>
2021-04-28 21:40:54 +0545

    moblile menu completed

---
+++ index.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".mobile-menu");
+const body = document.body;
+hamburger.addEventListener("click", () => {
+	mobileMenu.classList.toggle("open");
+	hamburger.classList.toggle("open-hamburger");
+	body.classList.toggle("hide-overflow");
+});
